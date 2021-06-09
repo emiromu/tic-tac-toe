00bee2ba83c27785a94ab5560b6078653c59fefa
Emilien Romulus <emilien.romulus@tuta.io>
2021-06-09 11:44:04 +1000

    Added reset board function; moved functions into gameBoard object; TODO:define the game flow object (gameRound)

--- engine.js
+++ engine.js
@@ -1,11 +1,11 @@
 
 /*
-012
-1
-2
-*/
+Board cells :
+036
+147
+258
 
-/* suits : 'o' 'x' */
+suits : 'o' 'x' */
 
 const playerFactory = (name, suit) => {
     const getName = () => name;
@@ -29,63 +29,78 @@ const gameBoard = (() => {
                 case 'o' :
                     document.querySelector(`#cell`+i).textContent = 'O';
                     break;
-                default : document.querySelector(`#cell`+i).textContent = /*''*/ gameBoard.cells[i];
+                default : document.querySelector(`#cell`+i).textContent = gameBoard.cells[i];
             }       
         }
     };
 
-    return {cells, tickCell, render};
-})();
-
-
-const isGameOver = function (gameBoard){
-    //check horizontal lines
-    for(let i=0; i<3; i++){
-        if(gameBoard.cells[i]!='' && (gameBoard.cells[i]===gameBoard.cells[i+3]&&gameBoard.cells[i]===gameBoard.cells[i+6])){
-            return `${gameBoard.cells[i]} wins`;
+    const isGameOver = () => {
+        //check horizontal lines
+        for(let i=0; i<3; i++){
+            if(gameBoard.cells[i]!='' && (gameBoard.cells[i]===gameBoard.cells[i+3]&&gameBoard.cells[i]===gameBoard.cells[i+6])){
+                console.log(`${gameBoard.cells[i]} wins`);
+                return `${gameBoard.cells[i]} wins`;
+            }
         }
-    }
-    //check vertical lines
-    for(let i=0; i<7; i=i+3){
-        if(gameBoard.cells[i]!='' && (gameBoard.cells[i]===gameBoard.cells[i+1]&&gameBoard.cells[i]===gameBoard.cells[i+2])){
-            return `${gameBoard.cells[i]} wins`;
+        //check vertical lines
+        for(let i=0; i<7; i=i+3){
+            if(gameBoard.cells[i]!='' && (gameBoard.cells[i]===gameBoard.cells[i+1]&&gameBoard.cells[i]===gameBoard.cells[i+2])){
+                console.log(`${gameBoard.cells[i]} wins`);
+                return `${gameBoard.cells[i]} wins`;
+            }
         }
-    }
-    //check horizontal lines
-    if(gameBoard.cells[0]!='' && (gameBoard.cells[0]===gameBoard.cells[4]&&gameBoard.cells[0]===gameBoard.cells[8])){
-        return `${gameBoard.cells[0]} wins`;
-    };
-    if(gameBoard.cells[2]!='' && (gameBoard.cells[2]===gameBoard.cells[4]&&gameBoard.cells[2]===gameBoard.cells[6])){
-        return `${gameBoard.cells[2]} wins`;
-    };
-
-    //check for draw
-    for(let i=0; i<gameBoard.cells.length; i++){
-        if(gameBoard.cells[i]==''){
-            return `ongoing`;
+        //check diagonal lines
+        if(gameBoard.cells[0]!='' && (gameBoard.cells[0]===gameBoard.cells[4]&&gameBoard.cells[0]===gameBoard.cells[8])){
+            console.log(`${gameBoard.cells[0]} wins`);
+            return `${gameBoard.cells[0]} wins`;
         };
+        if(gameBoard.cells[2]!='' && (gameBoard.cells[2]===gameBoard.cells[4]&&gameBoard.cells[2]===gameBoard.cells[6])){
+            console.log(`${gameBoard.cells[2]} wins`);
+            return `${gameBoard.cells[2]} wins`;
+        };
+        //check for draw
+        for(let i=0; i<gameBoard.cells.length; i++){
+            if(gameBoard.cells[i]==''){
+                return `ongoing`;
+            };
+        };
+        console.log(`draw`);
+        return `draw`;
     };
-    return `draw`;
-};
+
+    const boardReset = () =>{
+        for(let i=0; i<cells.length; i++){
+            cells[i]='';
+            document.querySelector(`#cell`+i).textContent = cells[i];
+        };
+        return 'board reset';
+    };
+
+    return {cells, tickCell, render, isGameOver, boardReset};
+})();
 
 const gameRound = (() => {
 
+    const computerPlayer = playerFactory('computer','x');
+    const humanPlayer = playerFactory("player",'o');
+
+
+    return {computerPlayer, humanPlayer};
 })();
 
-const computerPlayer = playerFactory('computer','x');
-const humanPlayer = playerFactory("player",'o');
-
+/*HTML elements to display the game board*/
 const displayBoard = document.querySelector("#displayBoard");
-
 
 /*event listeners for cells*/
 for(let i=0; i<9; i++){
     document.querySelector(`#cell`+i).addEventListener("click", function(e){
         if(gameBoard.cells[i]==''){
-            gameBoard.tickCell(i,humanPlayer.getSuit());
+            gameBoard.tickCell(i,gameRound.humanPlayer.getSuit());
         }   
         gameBoard.render();
+        gameBoard.isGameOver();
     });
 }
 
+//render on Page Load
 gameBoard.render();
